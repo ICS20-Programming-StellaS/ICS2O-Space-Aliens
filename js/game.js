e8c53cf9e3084cb600ee788e012c5c2bb5db84c7
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-06-01 14:13:38 +0000

    added comments to files

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Stella S All rights reserved
+//
+// Created by: Stella S
+// Created on: May 2022
+// This is the Phaser3 configuration file
+
+//*Game scence */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  }
+  //Set background color
   backgrondColor: 0x5f6e7a
+  scale: {
+  mode: Phaser.Scale.FIT
+  //We place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
